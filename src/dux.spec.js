4c168ee96c16ad5a92a2ad599c6e703cb5e9b175
etimoshenko <timoshenko.evgeny@gmail.com>
2018-02-06 22:14:44 +0100

    write correct test for adding and removing history entries

--- src/dux.spec.js
+++ src/dux.spec.js
@@ -20,6 +20,23 @@ function deepFreeze(o) {
   return o;
 }
 
+expect.extend({
+  toBeMoment(received, argument, granularity) {
+    const pass = received.isSame(argument);
+    if (pass) {
+      return {
+        message: () => `expected ${received} not to be same as ${argument}`,
+        pass: true
+      };
+    } else {
+      return {
+        message: () => `expected ${received} to be same as ${argument}`,
+        pass: false
+      };
+    }
+  }
+});
+
 describe('dux', () => {
   describe('completeRoutine', () => {
     it('should add todays date to the habit history', () => {
@@ -60,22 +77,23 @@ describe('dux', () => {
       );
 
       store.dispatch(Dux.addHistoryEntry(2, moment('19901230')));
-      expect(store.getState()).toMatchObject({
-        habits: [
-          {
-            _id: 1,
-            history: [{ when: moment('1990-12-30') }]
-          },
-          {
-            _id: 2,
-            history: [
-              { when: moment('1990-12-29') },
-              { when: moment('1990-12-30') },
-              { when: moment('1990-12-31') }
-            ]
-          }
-        ]
-      });
+      const habits = store.getState().habits;
+
+      expect(habits).toMatchObject([
+        {
+          _id: 1
+        },
+        {
+          _id: 2
+        }
+      ]);
+      expect(habits[0].history).toHaveLength(1);
+      expect(habits[0].history[0].when).toBeMoment(moment('19901230'))
+
+      expect(habits[1].history).toHaveLength(3);
+      expect(habits[1].history[0].when).toBeMoment(moment('19901229'))
+      expect(habits[1].history[1].when).toBeMoment(moment('19901230'))
+      expect(habits[1].history[2].when).toBeMoment(moment('19901231'))
     });
   });
 
@@ -100,7 +118,9 @@ describe('dux', () => {
           ]
         })
       );
+
       store.dispatch(Dux.removeHistoryEntry(2, moment('19901230')));
+
       expect(store.getState()).toMatchObject({
         habits: [
           {
@@ -108,14 +128,14 @@ describe('dux', () => {
             history: [{ when: moment('1990-12-30') }]
           },
           {
-            _id: 2,
-            history: [
-              { when: moment('1990-12-29') },
-              { when: moment('1990-12-31') }
-            ]
+            _id: 2
           }
         ]
       });
+      const history = store.getState().habits[1].history;
+      expect(history).toHaveLength(2);
+      expect(history[0].when).toBeMoment(moment('19901229'));
+      expect(history[1].when).toBeMoment(moment('19901231'));
     });
   });
 });
