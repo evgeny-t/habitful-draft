b54800e955cc137c74b95f50d54e51541ab9347e
etimoshenko <timoshenko.evgeny@gmail.com>
2018-02-12 23:38:36 +0100

    minor, formatting

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -4,50 +4,38 @@ import { withStyles } from 'material-ui/styles';
 
 const styles = theme => ({
   outer: {
-    border: '1px solid black',
+    '& svg': {
+      height: '100px',
+      width: '100%',
+    },
     '& svg text': {
       fontFamily: theme.typography.fontFamily,
-      fontSize: '10px',
-    },
+      fontSize: '10px'
+    }
   },
   rect: {
     fill: '#eeeeee',
     '&:hover': {
       stroke: 'black',
       strokeWidth: '1px',
-      shapeRendering: 'crispedges',
+      shapeRendering: 'crispedges'
     }
-  },
+  }
 });
 
-const daysOfWeek = [
-  'Su',
-  'Mo',
-  'Tu',
-  'We',
-  'Th',
-  'Fr',
-  'Sa',
-];
-
-const DARK_GREEN = "#217321";
-const GREY = '#eeeeee';
+const daysOfWeek = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
 export const Calendar = withStyles(styles)(
   class extends React.Component {
     static defaultProps = {
-      columns: 10,
+      columns: 20,
       itemSize: 10,
-      itemsDistance: 2,
-    }
+      itemsDistance: 2
+    };
 
     render() {
       const { outer, rect } = this.props.classes;
-      const { 
-        columns, 
-        itemSize,  
-        itemsDistance,
-      } = this.props;
+      const { columns, itemSize, itemsDistance } = this.props;
       const rects = [];
       const step = itemSize + itemsDistance;
       const paddingLeft = 24;
@@ -55,57 +43,58 @@ export const Calendar = withStyles(styles)(
       const fdow = moment(this.props.today).day(0);
 
       for (let i = 0; i < columns; ++i) {
-        const lastDayOfWeek = i === columns - 1 ?
-          this.props.today.day() : 6;
-        const currentWeekStarts = moment(fdow)
-          .day(-7 * (columns - 1 - i));
+        const lastDayOfWeek = i === columns - 1 ? this.props.today.day() : 6;
+        const currentWeekStarts = moment(fdow).day(-7 * (columns - 1 - i));
         for (let j = 0; j <= lastDayOfWeek; ++j) {
           const date = moment(currentWeekStarts).day(j);
-          const color = this.props.itemColor && 
-            this.props.itemColor(date)
+          const color = this.props.itemColor && this.props.itemColor(date);
           rects.push(
-            <rect 
+            <rect
               className={rect}
               key={`${i}-${j}-fill`}
-              width={itemSize} height={itemSize} 
+              width={itemSize}
+              height={itemSize}
               x={i * step + paddingLeft}
-              y={j * step + paddingTop} 
+              y={j * step + paddingTop}
               style={{ fill: color }}
             />
-          )
+          );
         }
       }
 
       for (let i = 1; i < 7; i += 2) {
-        rects.push((
-          <text key={`row_${i}`}
-            textAnchor='middle'
+        rects.push(
+          <text
+            key={`row_${i}`}
+            textAnchor="middle"
             x={paddingLeft >> 1}
-            y={i * step + itemSize * 0.9 + paddingTop}>
+            y={i * step + itemSize * 0.9 + paddingTop}
+          >
             {daysOfWeek[i]}
           </text>
-        ));
+        );
       }
 
       for (let i = columns - 1; i >= 0; i -= 2) {
         rects.push(
-          <text key={`col_${i}`}
-            textAnchor='middle'
+          <text
+            key={`col_${i}`}
+            textAnchor="middle"
             x={i * step + (itemSize >> 1) + paddingLeft}
-            y={paddingTop * 0.75}>
-            {moment(fdow).day(-7 * (columns - 1 - i)).date()}
+            y={paddingTop * 0.75}
+          >
+            {moment(fdow)
+              .day(-7 * (columns - 1 - i))
+              .date()}
           </text>
         );
       }
-      
+
       return (
         <div className={outer}>
-          <svg>
-            {rects}
-          </svg>
+          <svg>{rects}</svg>
         </div>
       );
     }
   }
 );
-
--- src/components/HabitCard.js
+++ src/components/HabitCard.js
@@ -1,29 +1,28 @@
-import _ from "lodash";
-import React from "react";
-import { render } from "react-dom";
-import moment from "moment";
+import _ from 'lodash';
+import React from 'react';
+import moment from 'moment';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import Typography from "material-ui/Typography";
-import Card, { CardActions, CardContent } from "material-ui/Card";
-import { withStyles } from "material-ui/styles";
+import Typography from 'material-ui/Typography';
+import Card, { CardContent } from 'material-ui/Card';
+import { withStyles } from 'material-ui/styles';
 
-import { Calendar } from "./Calendar";
+import { Calendar } from './Calendar';
 
-
-export const HabitCard = withStyles(theme => ({
-  H: {
-    // border: '2px solid black',
-    // width: 200,
-    margin: 10
-  },
-}), { withTheme: true })(
+export const HabitCard = withStyles(
+  theme => ({
+    card: {
+      margin: 10
+    }
+  }),
+  { withTheme: true }
+)(
   class extends React.Component {
     render() {
       const props = this.props;
       return (
-        <Card className={props.classes.H}>
+        <Card className={props.classes.card}>
           <CardContent>
             <Typography>
               <Link to={`/${_.kebabCase(props.routine)}`}>{props.routine}</Link>
@@ -40,14 +39,13 @@ export const HabitCard = withStyles(theme => ({
     getItemColor = date => {
       const props = this.props;
       // TODO(ET): should not be O(n)
-      if (props.history.find(x => x.when.isSame(date, "day"))) {
+      if (props.history.find(x => x.when.isSame(date, 'day'))) {
         return this.props.theme.palette.secondary['A200'];
       }
       if (date.isSame(props.today)) {
         console.log(date.format());
-        return "#000000";
+        return '#000000';
       }
     };
   }
-  );
-
+);
